1e3517ad85d1d12a7c4fedcbea574a3ca0ba0640
ubuntu-abeja <ubuntu@ubuntu.dev.abeja>
2016-09-26 17:57:13 +0900

    fix syntax error

--- layers/pool_where.py
+++ layers/pool_where.py
@@ -5,7 +5,7 @@ def getwhere(x):
     ''' Calculate the "where" mask that contains switches indicating which
     index contained the max value when MaxPool2D was applied.  Using the
     gradient of the sum is a nice trick to keep everything high level.'''
-                    y_prepool, y_postpool = x
+    y_prepool, y_postpool = x
     return K.gradients(K.sum(y_postpool), y_prepool)
 
 
